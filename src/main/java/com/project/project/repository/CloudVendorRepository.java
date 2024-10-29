976f6fabd85725379ec03a339da4030f33384e1e
patryk9901 <pszczolkowski99@gmail.com>
2024-10-29 22:46:43 +0100

    Basic Rest API

---
+++ src/main/java/com/project/project/repository/CloudVendorRepository.java
@@ -0,0 +1,8 @@
+package com.project.project.repository;
+
+import com.project.project.model.CloudVendor;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface CloudVendorRepository extends JpaRepository<CloudVendor, String> {
+
+}
---
+++ src/main/java/com/project/project/service/impl/CloudVendorServiceImpl.java
@@ -0,0 +1,44 @@
+package com.project.project.service.impl;
+
+import com.project.project.model.CloudVendor;
+import com.project.project.repository.CloudVendorRepository;
+import com.project.project.service.CloudVendorService;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+@Service
+public class CloudVendorServiceImpl implements CloudVendorService {
+
+    CloudVendorRepository cloudVendorRepository;
+    public CloudVendorServiceImpl(CloudVendorRepository cloudVendorRepository) {
+        this.cloudVendorRepository = cloudVendorRepository;
+    }
+
+    @Override
+    public String createCloudVendor(CloudVendor cloudVendor) {
+        cloudVendorRepository.save(cloudVendor);
+        return "Created";
+    }
+
+    @Override
+    public String updateCloudVendor(CloudVendor cloudVendor) {
+        cloudVendorRepository.save(cloudVendor);
+        return "Updated";
+    }
+
+    @Override
+    public String deleteCloudVendor(String cloudVendorId) {
+        cloudVendorRepository.deleteById(cloudVendorId);
+        return "Deleted";
+    }
+
+    @Override
+    public CloudVendor getCloudVendor(String cloudVendorId) {
+        return cloudVendorRepository.findById(cloudVendorId).get();
+    }
+
+    @Override
+    public List<CloudVendor> getAllCloudVendors() {
+        return cloudVendorRepository.findAll();
+    }
+}
